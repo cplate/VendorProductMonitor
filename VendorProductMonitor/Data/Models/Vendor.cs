b8f9b3a29cda23917b7a4252528ecde5a6507801
Chris Plate <cplate44@live.com>
2016-04-03 19:48:23 -0500

    Flip order of vendor name and code

--- VendorProductMonitor/Data/Models/Vendor.cs
+++ VendorProductMonitor/Data/Models/Vendor.cs
@@ -2,8 +2,8 @@
 {
     public class Vendor
     {
-        public string Code { get; set; }
         public string Name { get; set; }
+        public string Code { get; set; }
         public string Description { get; set; }
     }
 }
